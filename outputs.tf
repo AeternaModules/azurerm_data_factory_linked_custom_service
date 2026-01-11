98dbb16b5269910135ce2f10a9a6ecf7f0318dca
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:43:45 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_custom_services" {
+  description = "All data_factory_linked_custom_service resources"
+  value       = azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services
+}
+
