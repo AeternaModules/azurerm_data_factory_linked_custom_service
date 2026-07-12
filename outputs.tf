0acf3b091602d423f214d06c12528154eb36f335
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "data_factory_linked_custom_services" {
-  description = "All data_factory_linked_custom_service resources"
-  value       = azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services
-}
 output "data_factory_linked_custom_services_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.additional_properties }
 }
 output "data_factory_linked_custom_services_annotations" {
-  description = "List of annotations values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.annotations }
 }
 output "data_factory_linked_custom_services_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.data_factory_id }
 }
 output "data_factory_linked_custom_services_description" {
-  description = "List of description values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.description]
+  description = "Map of description values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.description }
 }
 output "data_factory_linked_custom_services_integration_runtime" {
-  description = "List of integration_runtime values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.integration_runtime]
+  description = "Map of integration_runtime values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.integration_runtime }
 }
 output "data_factory_linked_custom_services_name" {
-  description = "List of name values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.name]
+  description = "Map of name values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.name }
 }
 output "data_factory_linked_custom_services_parameters" {
-  description = "List of parameters values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.parameters }
 }
 output "data_factory_linked_custom_services_type" {
-  description = "List of type values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.type]
+  description = "Map of type values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.type }
 }
 output "data_factory_linked_custom_services_type_properties_json" {
-  description = "List of type_properties_json values across all data_factory_linked_custom_services"
-  value       = [for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : v.type_properties_json]
+  description = "Map of type_properties_json values across all data_factory_linked_custom_services, keyed the same as var.data_factory_linked_custom_services"
+  value       = { for k, v in azurerm_data_factory_linked_custom_service.data_factory_linked_custom_services : k => v.type_properties_json }
 }
 
